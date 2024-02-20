7cbe7f5c63e1f041cdfa73ca7275dd8e4e12c51f
Saeed Rezaei <saeed.rezaei1@gmail.com>
2024-02-20 04:06:39 +0330

    Add project files.

---
+++ src/Moon/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var chanel = connection.CreateModel();
+
+chanel.QueueDeclare("inbox", false, false, false, null);
+var consumer = new EventingBasicConsumer(chanel);
+consumer.Received += ConsumerEvent_Received;
+chanel.BasicConsume("inbox", true, consumer);
+Console.ReadKey();
+void ConsumerEvent_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(message);
+}
