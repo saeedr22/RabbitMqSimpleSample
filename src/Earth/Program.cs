ff85064eebcca31cb8e035871a7bfcdf374c9820
Saeed Rezaei <saeed.rezaei1@gmail.com>
2024-02-20 04:06:39 +0330

    Add sln file

---
+++ src/Earth/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var chanel = connection.CreateModel();
+
+chanel.QueueDeclare("inbox", false, false, false, null);
+var message = "";
+while (message == string.Empty)
+{
+    Console.WriteLine("please enter message");
+    message = Console.ReadLine();
+    var body = Encoding.UTF8.GetBytes(message);
+
+    chanel.BasicPublish("", "inbox", null, body);
+    Console.WriteLine("Message Sent from earth");
+    message = "";
+}
+
+
